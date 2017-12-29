f936db1f5288f75c02851122fdcc1016d0f9c94f
in-flu-ence <harrisngan@gmail.com>
2017-12-29 20:57:33 +0000

    Revised from submission comment
    - Plot3.png amended.
    - Plot4.R amended to remove kilowatts from top left graph y label.
    - Plot4.png amended

--- plot4.R
+++ plot4.R
@@ -20,7 +20,7 @@ data4$datetime <- as.POSIXct(paste(data4$Date, data4$Time), format="%d/%m/%Y %H:
 png('plot4.png')
 par(mfrow = c(2,2), mar = c(5,5,3,2))
 with(data4, {
-  plot(datetime, Global_active_power, type="l", xlab = "", ylab = "Global Active Power (kilowatts)")
+  plot(datetime, Global_active_power, type="l", xlab = "", ylab = "Global Active Power")
   plot(datetime, Voltage, type="l")
   plot(datetime, Sub_metering_1, type="l", xlab = "", ylab = "Energy sub metering")
   lines(datetime, Sub_metering_2, col="red")
